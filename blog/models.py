1574ad3da549fa0e66b3bb6c0d8c890a3a03c308
CA\xucao <seanxuncao@gmail.com>
2022-05-28 18:03:57 -0400

    update

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,12 @@ class Blog(models.Model):
     body = models.TextField()
     # image
     image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime(" %b %d, %Y ")
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def allblogs(request):
-    blogs="my blogs"
+    blogs=Blog.objects.all
     return render(request,"blog/allblog.html",{"blogs":blogs})
